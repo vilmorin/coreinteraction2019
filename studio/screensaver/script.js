539224dae60ef8c1403d6d9da110ffbc6a4c0ac2
Emiliano Flores <42790639+vilmorin@users.noreply.github.com>
2019-05-02 20:28:15 -0400

    Add files via upload

---
+++ studio/screensaver/script.js
@@ -0,0 +1,37 @@
+function beep() {
+  (new
+	Audio("audio/2.1.mp3")).play();
+}
+
+
+
+
+
+// var buildingNumber = 30;
+// var docHeight = $(document).height();
+// var docWidth = $(document).width();
+
+
+// setInterval(function() {
+
+	// for (i=0; i < 30; i++){
+
+	// 	var randomHeight = Math.random()*docHeight;
+	// 	var randomWidth = Math.random()*docWidth;
+
+	// 	var randomSizeH = Math.random()* (docHeight/3);
+	// 	var random = Math.random()* (docHeight);
+
+
+	// 	var	randomShape = shapes[ Math.floor(Math.random() * shapes.length) ]
+
+	// 	var clone = $("#shape").clone().appendTo('body');
+	// 	clone.last().css('height', random);
+	// 	clone.last().css('width', randomWidth);
+	// 	clone.last().css('top', randomHeight);
+	// 	clone.last().css('left', random);
+
+	// 	console.log(random + 1);
+	// }
+
+// },1000)
